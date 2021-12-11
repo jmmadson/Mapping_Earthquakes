7788d96fcadbbba08987ea2ac3fde020e10af142
jmmadson <jmmadson@gmail.com>
2021-12-11 12:10:15 -0600

    committing at 13.5.3

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -8,15 +8,23 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// Add our 'streets' tile layer to the map. 
+streets.addTo(map);
+
 // Add GeoJSON data.
 let airportData = "https://raw.githubusercontent.com/jmmadson/Mapping_Earthquakes/main/majorAirports.json";
 
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
   console.log(data);
-  
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+
+  L.geoJson(data, {
+      onEachFeature: function(features, layer){
+      layer.bindPopup("<h2> Airport code: " + features.properties.faa + "<hr>" + "Airport Name: " + features.properties.name + ", " + features.properties.country + "</h2>")
+    }
+  }).addTo(map);
+
 });
-
-
