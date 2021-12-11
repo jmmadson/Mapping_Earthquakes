4dbef6aa99979c9d2b1b534ab3d5b12b1ea36e51
jmmadson <jmmadson@gmail.com>
2021-12-11 12:26:58 -0600

    adding files

---
+++ Mapping_GeoJSON_Lines/static/js/logic.js
@@ -0,0 +1,51 @@
+// Loop through the cities array and create one marker for each city.
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create basemaps layer
+
+let baseMaps = {
+  Light: light,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// // Add our 'streets' tile layer to the map. 
+// streets.addTo(map);
+
+// Add GeoJSON data.
+let airportData = "https://raw.githubusercontent.com/jmmadson/Mapping_Earthquakes/main/majorAirports.json";
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
+
+  L.geoJson(data, {
+      onEachFeature: function(features, layer){
+      layer.bindPopup("<h2> Airport code: " + features.properties.faa + "<hr>" + "Airport Name: " + features.properties.name + ", " + features.properties.country + "</h2>")
+    }
+  }).addTo(map);
+
+});
